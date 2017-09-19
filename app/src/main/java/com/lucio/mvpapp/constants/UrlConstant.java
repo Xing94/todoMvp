7e5cc7baae8e51f2ce3359f7b0b17090d5e9f5e8
Xing94 <1115119438@qq.com>
2017-09-19 16:28:55 +0800

    Update UrlConstant.java

--- app/src/main/java/com/lucio/mvpapp/constants/UrlConstant.java
+++ app/src/main/java/com/lucio/mvpapp/constants/UrlConstant.java
@@ -8,6 +8,5 @@ import retrofit2.http.POST;
 
 public interface UrlConstant {
 
-    //?phone=13122576190&loginPwd=24cfeed0c1c1d9ed3414afc78d096b67
     String login = "SleepDetector/client/user/login";
 }
